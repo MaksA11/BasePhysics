a49a9d5b6876a8871ce5fd7776ae913cd266c1dc
MaksA11 <kamionka27@gmail.com>
2025-12-23 16:32:01 +0100

    Libraries

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,40 @@
-#include <iostream>
+#include <SFML/Audio.hpp>
+#include <SFML/Graphics.hpp>
+#include <imgui.h>
+#include <imgui-SFML.h>
 
 int main()
 {
-    std::cout << "Hello, World!";
-    return 0;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+    window.setFramerateLimit(60);
+
+    ImGui::SFML::Init(window);
+
+    sf::Clock deltaClock;
+
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            ImGui::SFML::ProcessEvent(window, event);
+
+            if(event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        ImGui::SFML::Update(window, deltaClock.restart());
+
+        ImGui::Begin("Hello");
+        ImGui::Text("Hello, World!");
+        ImGui::End();
+
+        window.clear();
+        ImGui::SFML::Render(window);
+        window.display();
+    }
+
+    ImGui::SFML::Shutdown();
+
+    return EXIT_SUCCESS;
 }
